9b68f992d07a5f947150eafe0264afed9ceef775
Samarth292005 <samarthagarwal6277@gmail.com>
2026-07-11 20:05:52 +0530

    Update fresh.java

--- recursion/fresh.java
+++ recursion/fresh.java
@@ -389,5 +389,15 @@ class Solution {
      return c;
     }
 }
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+}
 
 }
