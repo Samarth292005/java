37c01dfe0bd6e94ca74b845c65ef68bc18d0775d
Samarth292005 <samarthagarwal6277@gmail.com>
2026-07-12 20:16:59 +0530

    Update fresh.java

--- recursion/fresh.java
+++ recursion/fresh.java
@@ -410,5 +410,29 @@ class Solution {
         return ans;
     }
 }
+import java.util.*;
 
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
+        HashMap<Integer, Integer> rank = new HashMap<>();
+        int r = 1;
+
+        // Assign ranks to unique elements
+        for (int num : sorted) {
+            if (!rank.containsKey(num)) {
+                rank.put(num, r++);
+            }
+        }
+
+        // Replace each element with its rank
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rank.get(arr[i]);
+        }
+
+        return arr;
+    }
+}
 }
