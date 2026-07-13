05e2eb12d166a780d3c15f4320d6f4c7b8dc58a9
Samarth292005 <samarthagarwal6277@gmail.com>
2026-07-13 22:16:43 +0530

    Add method to reverse bits in an integer

--- recursion/fresh.java
+++ recursion/fresh.java
@@ -435,4 +435,17 @@ class Solution {
         return arr;
     }
 }
+class Solution {
+    public int reverseBits(int n) {
+        int ans = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int lastBit = n & 1;
+            ans = (ans << 1) | lastBit;
+            n >>>= 1;      // unsigned right shift
+        }
+
+        return ans;
+    }
+}
 }
