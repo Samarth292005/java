57c394f99d355d6fec7a338703b2f51cd1af89f8
Samarth292005 <samarthagarwal6277@gmail.com>
2026-07-24 22:37:22 +0530

    Update fresh.java

--- recursion/fresh.java
+++ recursion/fresh.java
@@ -823,5 +823,32 @@ class Solution {
         }
     }
 }
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        final int MAX = 2048;
+
+        boolean[][] dp = new boolean[4][MAX];
+        dp[0][0] = true;
+
+        for (int num : nums) {
+            for (int cnt = 3; cnt >= 1; cnt--) {
+                for (int x = 0; x < MAX; x++) {
+                    if (dp[cnt - 1][x]) {
+                        dp[cnt][x ^ num] = true;
+                    }
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int x = 0; x < MAX; x++) {
+            if (dp[1][x] || dp[3][x]) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
                         }
 }
