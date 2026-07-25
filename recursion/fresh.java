d54d848b90ce8c4fc4bb0e1b9257c859ae213af3
Samarth292005 <samarthagarwal6277@gmail.com>
2026-07-25 23:38:19 +0530

    Implement maxProduct method in Solution class
    Added a new method to calculate the maximum product of two digits in an integer.

--- recursion/fresh.java
+++ recursion/fresh.java
@@ -849,6 +849,27 @@ class Solution {
 
         return ans;
     }
+    class Solution {
+    public int maxProduct(int n) {
+        int max1 = 0;
+        int max2 = 0;
+        while(n != 0)
+        {
+            int k = n % 10;
+            n = n/10;
+            if(k>=max1)
+            {
+                max2 = max1;
+                max1 = k;
+            }
+            else if(k>max2 && k<max1)
+            {
+                max2 = k;
+            }
+        }
+        return max1*max2;
+    }
+}
 }
                         }
 }
