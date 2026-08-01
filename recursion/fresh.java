df980c0f95d5fe55cb14c5aad653fe357975ee58
Samarth292005 <samarthagarwal6277@gmail.com>
2026-08-01 23:41:59 +0530

    Implement PredictTheWinner solution with memoization

--- recursion/fresh.java
+++ recursion/fresh.java
@@ -985,6 +985,31 @@ class Solution {
         return ans;
     }
 }
+}class Solution {
+    int[][] dp;
+
+    public boolean PredictTheWinner(int[] nums) {
+        int n = nums.length;
+        dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], Integer.MIN_VALUE);
+        }
+
+        return solve(0, n - 1, nums) >= 0;
+    }
+
+    private int solve(int i, int j, int[] nums) {
+        if (i == j) return nums[i];
+
+        if (dp[i][j] != Integer.MIN_VALUE)
+            return dp[i][j];
+
+        int left = nums[i] - solve(i + 1, j, nums);
+        int right = nums[j] - solve(i, j - 1, nums);
+
+        return dp[i][j] = Math.max(left, right);
+    }
 }
                         }
 }
