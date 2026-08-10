febb0bbc03c58a8a80cd5fc7ed34745ba9da9ade
Samarth292005 <samarthagarwal6277@gmail.com>
2026-08-10 22:49:10 +0530

    Update fresh.java

--- recursion/fresh.java
+++ recursion/fresh.java
@@ -1194,6 +1194,31 @@ class Solution {
         return dp[0][1];
     }
 }
+    class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int num : nums1) {
+            set.add(num);
+        }
+
+        for (int num : nums2) {
+            if (set.contains(num)) {
+                list.add(num);
+                set.remove(num);
+            }
+        }
+
+        int[] ans = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+
+        return ans;
+    }
+}
 }
                         }
 }
