16ff5bbeddbc4aef031249a6853a9c72a2a8e32b
Samarth292005 <samarthagarwal6277@gmail.com>
2026-08-15 01:11:28 +0530

    Add maximumLengthSubstring method in Solution class
    Implement maximumLengthSubstring method to find the length of the longest substring with at most two distinct characters.

--- recursion/fresh.java
+++ recursion/fresh.java
@@ -1288,6 +1288,26 @@ class Solution {
         return true;
     }
 }
+    class Solution {
+    public int maximumLengthSubstring(String s) {
+        int[] freq = new int[26];
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            freq[s.charAt(right) - 'a']++;
+
+            while (freq[s.charAt(right) - 'a'] > 2) {
+                freq[s.charAt(left) - 'a']--;
+                left++;
+            }
+
+            ans = Math.max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+}
 }
                         }
 }
