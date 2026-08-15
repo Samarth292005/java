2b72163697ad297f1e02123619eb527b8fbf196e
Samarth292005 <samarthagarwal6277@gmail.com>
2026-08-16 01:28:45 +0530

    Add minSubArrayLen method to Solution class
    Implemented a method to find the minimum length of a subarray with a sum greater than or equal to a target value.

--- recursion/fresh.java
+++ recursion/fresh.java
@@ -1308,6 +1308,25 @@ class Solution {
         return ans;
     }
 }
+    class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int low = 0;
+        int sum = 0;
+        int res = Integer.MAX_VALUE;
+
+        for (int high = 0; high < nums.length; high++) {
+            sum += nums[high];
+
+            while (sum >= target) {
+                res = Math.min(res, high - low + 1);
+                sum -= nums[low];
+                low++;
+            }
+        }
+
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+}
 }
                         }
 }
