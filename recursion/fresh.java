10ba115189779fcf0aa79a8dbd5b0d169c234464
Samarth292005 <samarthagarwal6277@gmail.com>
2026-08-18 23:16:19 +0530

    Add pivotIndex method to Solution class

--- recursion/fresh.java
+++ recursion/fresh.java
@@ -1383,6 +1383,30 @@ class Solution {
         return count;
     }
 }
+    class Solution {
+    public int pivotIndex(int[] nums) {
+        int p[] = new int[nums.length];
+        int s[] = new int[nums.length];
+        p[0] = nums[0];
+        s[nums.length-1] = nums[nums.length-1];
+        for(int i=1;i<nums.length;i++)
+        {
+            p[i] = p[i-1] + nums[i];
+        }
+          for(int i=nums.length-2;i>=0;i--)
+        {
+            s[i] = s[i+1] + nums[i];
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(p[i]==s[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
 }
                         }
 }
