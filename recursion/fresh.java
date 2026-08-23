b0dd5366e947bc2a9c41f40fdef566af69c5a435
Samarth292005 <samarthagarwal6277@gmail.com>
2026-08-24 02:03:01 +0530

    Update fresh.java

--- recursion/fresh.java
+++ recursion/fresh.java
@@ -1,4 +1,20 @@
 package recursion;
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int num : nums) {
+            pq.add(num);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        return pq.peek();
+    }
+}
 class fresh{
     public static void main(String[] args)
     {
