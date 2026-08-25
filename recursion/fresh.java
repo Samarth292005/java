0b570b6ff773013c02707dd49bc10109387ad62f
Samarth292005 <samarthagarwal6277@gmail.com>
2026-08-26 02:57:43 +0530

    Implement missingMultiple method in Solution class
    Add method to find the first missing multiple of k in an array.

--- recursion/fresh.java
+++ recursion/fresh.java
@@ -1,5 +1,24 @@
 package recursion;
 class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        for (int i = 1; ; i++) {
+            int ele = i * k;
+            boolean found = false;
+
+            for (int j = 0; j < nums.length; j++) {
+                if (ele == nums[j]) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                return ele;
+            }
+        }
+    }
+}
+class Solution {
     public int thirdMax(int[] nums) {
         Long first = null;
         Long second = null;
